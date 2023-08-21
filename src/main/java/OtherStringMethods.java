1dc60a3a234db890144bfd3ff2cedc01e586fcb3
Hoa Do <hoa.do.ma@gmail.com>
2023-08-21 21:32:54 +0000

    Completed Lab: Other String Methods

--- src/main/java/OtherStringMethods.java
+++ src/main/java/OtherStringMethods.java
@@ -11,7 +11,7 @@ public class OtherStringMethods {
      * @return
      */
     public String partOfString(String s, int start, int end){
-        return "";
+        return s.substring(start, end);
     }
 
     /**
@@ -26,6 +26,12 @@ public class OtherStringMethods {
      * and 0 if s1 is equivalent to s2.
      */
     public int compareLexigraphically(String s1, String s2) {
+        int result = s1.compareTo(s2);
+        if (result < 0) {
+            return -1;
+        } else if ( result > 0) {
+            return 1;
+        }
         return 0;
     }
 
@@ -39,6 +45,6 @@ public class OtherStringMethods {
      * @return an array of Strings that represent s1 split by splitAround.
      */
     public String[] splitStringIntoMultipleStrings(String s1, String splitAround) {
-        return null;
+        return s1.split(splitAround);
     }
 }
